c788cd274213abb6d38203cb3c39dabf95f09916
Thiago <146882957+thigszuza@users.noreply.github.com>
2026-09-23 18:43:56 -0300

    Correções
    Fix: Adiciona extend_existing no modelo Job

--- models.py
+++ models.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from typing import Optional
 from sqlmodel import Field, SQLModel
@@ -7,19 +6,20 @@ from sqlmodel import Field, SQLModel
 class JobBase(SQLModel):
   title: str
   hospital_or_company: str
-  location: str = "Belém, PA"
-  neighborhood: Optional[str] = None
+  location: str
   shift_type: Optional[str] = None
-  specialty: Optional[str] = None
+  specialty: Optional[str] = "Geral"
   salary: Optional[float] = None
-  description: str
+  description: Optional[str] = None
   url_apply: str
-  source: str
-  created_at: datetime = Field(default_factory=datetime.utcnow)
+  source: Optional[str] = "Web"
 
 
 class Job(JobBase, table=True):
+  __table_args__ = {"extend_existing": True}
+
   id: Optional[int] = Field(default=None, primary_key=True)
+  created_at: datetime = Field(default_factory=datetime.utcnow)
 
 
 class JobCreate(JobBase):
